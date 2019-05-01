c8409f9e7584811b3e72ad6ed259e8af28a39694
nicolashermosilla <nhermosilla1987@gmail.com>
2019-05-01 03:49:15 -0400

    avances

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// Updated. Thanks to: Paul Luna
 import React, { Component } from "react";
 import io from "socket.io-client";
 const socket = io("localhost:5000");
@@ -20,9 +19,6 @@ class App extends Component {
 
   componentDidMount = () => {
 
-    setInterval(this.send, 3000);
-    
-
     socket.on('setCities', (data) => {
       console.log(data);
         this.setState({cities: data});
@@ -32,16 +28,25 @@ class App extends Component {
   render() {
 
     const cities = this.state.cities;
-
+/*
+TODO: REQ-1:
+Se desea mostrar en pantalla completa la hora y la temperatura y hora de las siguientes ciudades:
+Santiago (CL), Zurich (CH), Auckland (NZ), Sydney (AU), Londres (UK), Georgia (USA)
+*/
     return (
       <div>
          {cities.map((item)=> {
+
+          let timeZone = new Date(new Date().toLocaleString("en-US", {timeZone: item.timezone}));
+
            return (
           <div>
-          <div>Ciudad: {item.name}{item.now}</div>
+          <div>Ciudad: {item.name}</div>
           <ul>
-          <li>Time: {new Date(item.time).toTimeString()}</li>
-          <li>Temperature: {item.temperature}</li>  
+          <li>Time: {timeZone.toLocaleString()}</li>
+          <li>Temperature F: {item.temperature}</li>  
+          <li>Temperature C: {(item.temperature - 32) * 5/9}</li>  
+          
           </ul>
           </div>)
         })}
--- index.js
+++ index.js
@@ -1,115 +1,214 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var async = require("async");
-const request = require('request');
-const fetch = require('node-fetch');
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+const request = require("request");
+const redis = require("redis");
+const citiesInfo = require("./cities.json");
 
-var cities= [
-    {
-        id: "santiago-cl",
-        name: "Santiago (CL)",
-        latitude: -33.4569397,
-        longitude: -70.6482697
-    },
-    {
-        id: "zurich-ch",
-        name: "Zurich (CH)",
-        latitude: 47.3666687,
-        longitude: 8.5500002
-    },
-    {
-        id: "auckland-nz",
-        name: "Auckland (NZ)",
-        latitude: -36.8484597,
-        longitude: 174.7633315
-    },
-    {
-        id: "sydney-au",
-        name: "Sydney (AU)",
-        latitude: 46.1351013,
-        longitude: -60.1831017
-    },
-    {
-        id: "londres-uk",
-        name: "Londres (UK)",
-        latitude: 51.5127907,
-        longitude: -0.09184
-    },
-    {
-        id: "georgia-usa",
-        name: "Georgia (USA)",
-        latitude: 31.6371002,
-        longitude: -86.7419205
-    }
-];
+const REDIS_KEY_CITY = "api:cities";
+const REDIS_HASH_KEY_ERROR = "api:erros";
+const REDIS_HOST = "cities-001.8zlanq.0001.use2.cache.amazonaws.com";
+const API_KEY = "fe0c04d83b6b1db087328c76a8f43c60";
+const API_URL = "https://api.darksky.net/forecast/";
+const ERROR_MSG = "How unfortunate! The API Request Failed";
+const MAX_RETRY = 5;
+const TIME_UPDATE_CLIENT = 10000;
+const TIME_RETRY = 500;
 
-const API_KEY ="fe0c04d83b6b1db087328c76a8f43c60";
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+//static files react app
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('/cities', async (req, res) => {
-    
-    let _data = await getRequestData();
-    console.log(_data);
-      res.send(_data);
-    
-  })
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-io.on('connection', async(socket) => {
-    console.log('a user connected');
- 
-    let _data = await getRequestData();
-    console.log(_data);
-    io.emit('setCities',_data );
+//api routes
+app.get("/cities", async (req, res) => {
+  let data = await getRequestData();
+  res.send(data);
+});
 
-    socket.on('getCities', async() => {
-      console.log('getCities');
-      let _data = await getRequestData();
-      io.emit('setCities',_data );
-      
+app.get("/cities-redis", async (req, res) => {
+  const data = await getCitiesFromRedis();
+  res.send(data);
+});
 
-    });
-  
+app.get("/errors", (req, res) => {
+  redisclient.HGETALL(REDIS_HASH_KEY_ERROR, function(err, data) {
+    if (err) res.send(err);
+    else res.send(data);
   });
-  
+});
 
+//socket io
+/*
+TODO: REQ-6:
+El frontend deberá actualizarse cada 10 segundos a través de web sockets. 
+*/
+io.on("connection", async socket => {
+  console.log("a user connected");
+
+  let _data = await getRequestData();
+  console.log(_data);
+  io.emit("setCities", _data);
+
+  setInterval(async () => {
+    let _data = await getRequestData();
+    io.emit("setCities", _data);
+  }, TIME_UPDATE_CLIENT);
+
+  socket.on("getCities", async () => {
+    console.log("getCities");
+  });
+});
+
+//redis
+const redisclient = redis.createClient();
+
+redisclient.on("connect", function() {
+  console.log("Redis client connected");
+});
+
+redisclient.on("error", function(err) {
+  console.log("Something went wrong " + err);
+});
+
+//server startup
 const port = process.env.PORT || 5000;
-server.listen(port);
 
-console.log('App is listening on port ' + port);
+server.listen(port, () => {
 
-async function getRequestData()
-{
-    
-    const cities =  getCitiesData();
+/*
+TODO: REQ-2:
+Las latitudes y longitudes de cada ciudad deben ser guardadas en Redis al momento de iniciar la aplicación.
+*/
+  redisclient.set(REDIS_KEY_CITY, JSON.stringify(citiesInfo), redis.print);
+
+  console.log("Server listening on port: ", port);
+});
+
+/*
+TODO: REQ-3:
+Cada request de la API debera ir a Redis, sacar las latitudes y longitudes correspondientes, y hacer las consultas necesarias al servicio de Forecast.io.
+*/
+const getRequestData = async () => {
+  const cities = await getCitiesFromRedis();
+
+  //Get all city data from api
+  const result = await Promise.all(
+    cities.map(async city => {
+
+      const url = `${API_URL}${API_KEY}/${city.latitude},${city.longitude}`;
+
+      let cityInfo = { ...city };
+
+      try {
+
+        const response = await retry(requestAsync, url);
+        const { timezone } = response;
+        const { temperature } = response.currently;
+
+        cityInfo.timezone = timezone;
+        cityInfo.temperature = temperature;
+        cityInfo.time = +new Date();//last update
+
+/*
+TODO: REQ-6.1:
+El proceso deberá actualizar redis y luego enviar el update al frontend.
+*/
+        redisclient.set(cityInfo.id, JSON.stringify(cityInfo));
 
 
-    var result = await Promise.all(cities.map(async(city) => {
+      } catch (error) {
+        //obtener ultimo valor desde redis si supero todos los intentos de llamada a la api**plus
+        let _cityInfo = await getCityFromRedis(city.id);      
 
-        let url = `https://api.darksky.net/forecast/${API_KEY}/${city.latitude},${city.longitude}`;     
-        let data = await fetch(url).then((response) => response.json());
-        return {...data.currently,...city, now:new Date()};
-    }));
+        if(_cityInfo)
+         return _cityInfo
+    }
 
-    return result;
-}
-
-requestAsync = (url) => 
-    new Promise((resolve, reject) => {
-        request(url, (error, response, data) => {
-            if(error) reject(error)
-            else resolve(JSON.parse(data))
-        })
+      return cityInfo;
     })
+  );
 
-function getCitiesData(){
+  return result;
+};
 
-    //TODO: Migrar a redis
-    return cities;
-}
+/*
+TODO: REQ-5:
+Esto nos simulara un fallo del 10%~, la aplicacion debera rehacer el request las veces que sea necesario para tener una respuesta correcta
+cada fallo deberá guardarse en Redis dentro de un hash llamado "api.errors", la llave debera ser el timestamp y el contenido debe ser relevante al error. El handler de error deberá capturar solamente este error y no otro con diferente clase o mensaje.
+Comment: Se deja una cantidad limitada de reintentos en MAX_RETRY cada x tiempo definido en TIME_RETRY
+*/
+const retry = (fn, url, ms = TIME_RETRY, retriesLeft = MAX_RETRY) =>
+  new Promise((resolve, reject) => {
+    var retries = 0;
+    fn(url)
+      .then(resolve)
+      .catch(e => {
+        setTimeout(() => {
+/*
+TODO: REQ-5.1:
+cada fallo deberá guardarse en Redis dentro de un hash llamado "api.errors", la llave debera ser el timestamp y el contenido debe ser relevante al error. 
+El handler de error deberá capturar solamente este error y no otro con diferente clase o mensaje.
+*/          if (e instanceof Error && e.message === ERROR_MSG) {
+            console.log("Error 10% :)");
+            redisclient.hset(
+              REDIS_HASH_KEY_ERROR,
+              +new Date(),
+              `ERROR: ${e.message} - REQUEST: ${url}`
+            );
+          }
+          retries++;
+          console.log(`retries ${retriesLeft}`);
+          if (retriesLeft === 1) {
+            return reject("maximum retries exceeded");
+          }
+          retry(fn, url, ms, retriesLeft - 1).then(resolve, reject);
+        }, ms);
+      });
+  });
+
+const requestAsync = url =>
+  new Promise((resolve, reject) => {
+
+/*
+TODO: REQ-4:
+Cada request a la API tiene un 10% de chances de fallar, al momento de hacer el request deberá suceder lo siguiente:
+if (Math.rand(0, 1) < 0.1) throw new Error('How unfortunate! The API Request Failed')
+*/
+    if (Math.random(0, 1) < 0.1) throw new Error(ERROR_MSG);
+
+    request(url, (error, response, result) => {
+      {
+        if (typeof result !== "object") {
+          reject(result);
+          return;
+        }
+
+        error ? reject(error) : resolve(JSON.parse(result));
+      }
+    });
+  });
+
+  
+const getCitiesFromRedis = () =>
+  new Promise((resolve, reject) => {
+    redisclient.get(REDIS_KEY_CITY, (error, result) => {      
+        error ? reject(error) : resolve(JSON.parse(result));
+      });
+  });
+
+  const getCityFromRedis = (cityId) =>
+  new Promise((resolve, reject) => {
+    redisclient.get(cityId, (error, result) => {
+      
+        console.log(error);
+        console.log(result);
+        error ? reject(error) : resolve(JSON.parse(result));
+      
+    });
+  });
